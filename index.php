11e34423229288d1214a0cd4c02bad951b77b0fc
Fabio Uchelli <fabiouchelli@hotmail.com>
2019-06-30 23:12:23 -0300

    Acessando lista de usuários no BD

--- index.php
+++ index.php
@@ -6,7 +6,19 @@ $dbpass = "";
 
 try{
 	$pdo = new PDO($dsn, $dbuser, $dbpass);
-	echo "Conexão estabelecida com sucesso";
+
+	$sql = "select * from usuarios";
+	$sql = $pdo->query($sql);
+
+	if($sql->rowCount() > 0){
+		foreach($sql->fetchAll() as $usuario){
+			echo "Nome: ".$usuario["email"]."<br>";
+		}
+
+	}else{
+		echo "Não há usuários cadastrados";
+	}
+
 }catch(PDOException $e){
 	echo "Falhou: ".$e->getMessage();
 }
