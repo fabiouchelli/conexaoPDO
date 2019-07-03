8fbea865903d9b49f9fe3f5d58c42e1924ad8fc1
Fabio Uchelli <fabiouchelli@hotmail.com>
2019-07-03 15:00:53 -0300

    PDOStatement e binds

--- index.php
+++ index.php
@@ -7,19 +7,29 @@ $dbpass = "";
 try{
 	$pdo = new PDO($dsn, $dbuser, $dbpass);
 
-	$nome = "Fulano";
+	$nome = 'Katia';
+	$id = 5;
+
+	$sql = "UPDATE usuarios SET nome = :nome WHERE id = :id";
+
+	$sql = $pdo->prepare($sql);
+	$sql->bindValue(':nome', $nome);
+	$sql->bindValue(':id', $id);
+	$sql->execute();
+
+	/*$nome = "Fulano";
 	$email = "fulano@hotmail.com";
 	$senha = md5("123");
 
-	//$sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')"; 
-	//$sql = "INSERT INTO usuarios SET nome = '$nome', email = '$email', senha = '$senha'"; 
-	/*$sql = "UPDATE usuarios SET email = 'abc@hotmail.com' WHERE email = 'fulano@hotmail.com'";
-	$sql = $pdo->query($sql);*/
+	$sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')"; 
+	$sql = "INSERT INTO usuarios SET nome = '$nome', email = '$email', senha = '$senha'"; 
+	$sql = "UPDATE usuarios SET email = 'abc@hotmail.com' WHERE email = 'fulano@hotmail.com'";
+	$sql = $pdo->query($sql);
 	$sql = "DELETE FROM usuarios WHERE id = 10";
-	$pdo->query($sql);
+	$pdo->query($sql);*/
 
-	echo "Usuário deletado com sucesso!";
-	//echo "Usuário inserido: ".$pdo->lastInsertId();
+	echo "Usuário alterado com sucesso!";
+	//echo "Usuário inserido: ".$pdo->lastInsertId(); //busca o ultimo id inserido no bd
 
 }catch(PDOException $e){
 	echo "Falhou: ".$e->getMessage();
@@ -27,30 +37,4 @@ try{
 
 ?>
 
-<?php
-
-//buscando dados no bd
-/*$dsn = "mysql:dbname=testepdo;host=localhost"; 
-$dbuser = "root";
-$dbpass = "";
-
-try{
-	$pdo = new PDO($dsn, $dbuser, $dbpass);
-
-	$sql = "select * from usuarios";
-	$sql = $pdo->query($sql);
-
-	if($sql->rowCount() > 0){
-		foreach($sql->fetchAll() as $usuario){
-			echo "Nome: ".$usuario["nome"]."<br>";
-		}
-
-	}else{
-		echo "Não há usuários cadastrados";
-	}
-
-}catch(PDOException $e){
-	echo "Falhou: ".$e->getMessage();
-}
-?>*/
 
