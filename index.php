1b94384f2487bf87a96dedd3c5ba4144fd416f9d
Fabio Uchelli <fabiouchelli@hotmail.com>
2019-07-03 14:31:03 -0300

    alterando informações no banco de dados

--- index.php
+++ index.php
@@ -12,10 +12,12 @@ try{
 	$senha = md5("123");
 
 	//$sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')"; 
-	$sql = "INSERT INTO usuarios SET nome = '$nome', email = '$email', senha = '$senha'"; 
+	//$sql = "INSERT INTO usuarios SET nome = '$nome', email = '$email', senha = '$senha'"; 
+	$sql = "UPDATE usuarios SET email = 'abc@hotmail.com' WHERE email = 'fulano@hotmail.com'";
 	$sql = $pdo->query($sql);
 
-	echo "Usuário inserido: ".$pdo->lastInsertId();
+	echo "Email alterado com sucesso!";
+	//echo "Usuário inserido: ".$pdo->lastInsertId();
 
 }catch(PDOException $e){
 	echo "Falhou: ".$e->getMessage();
@@ -24,6 +26,7 @@ try{
 ?>
 
 <?php
+
 //buscando dados no bd
 /*$dsn = "mysql:dbname=testepdo;host=localhost"; 
 $dbuser = "root";
